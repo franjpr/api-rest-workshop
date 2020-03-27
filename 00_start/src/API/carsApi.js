6540dae59672a12a295379a0d3b8d1d17b9d011f
franjpr <franpr90@gmail.com>
2020-03-27 20:13:28 +0000

    added axios requests

--- 00_start/src/API/carsApi.js
+++ 00_start/src/API/carsApi.js
@@ -1,1 +1,16 @@
+const axios = require('axios').default;
+
 const baseUrl = 'http://localhost:3050';
+const apiEndpoint = '/api/cars/';
+
+export const getAllCarsWithAxios = () => {
+  return axios.get(`${baseUrl}${apiEndpoint}`)
+}
+
+export const getCarByIdWithAxios = (id) => {
+  return axios.get(`${baseUrl}${apiEndpoint}${id}`);
+}
+
+export const addCarWithAxios = (data) => {
+  return axios.post(`${baseUrl}${apiEndpoint}`, data);
+}
--- 00_start/src/app.js
+++ 00_start/src/app.js
@@ -1,18 +1,54 @@
-import { 
-    addCarRows, 
-    retrieveCarId, 
+import {
+    addCarRows,
+    retrieveCarId,
     populateEditCarForm,
     retrieveCarForm,
     cleanTable,
 } from './uiHelpers';
-import { 
-   getAllCars,
-   getCarById,
-   addCar 
+import {
+    getAllCars,
+    getCarById,
+    addCar
 } from './API/carsApi.double';
+import { getAllCarsWithAxios, getCarByIdWithAxios, addCarWithAxios } from './API/carsApi';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    ///////////////////////////
+    // Load data by axios    //
+    ///////////////////////////
+    const buttonLoadCarsAxios = document.getElementById('loadcarsaxios');
+    const buttonLoadCarAxios = document.getElementById('loadcaraxios');
+    const buttonAddCarAxios = document.getElementById('addaxios');
+
+    buttonLoadCarsAxios.addEventListener('click', (event) => {
+        event.stopPropagation();
+        cleanTable('cars-table');
+        getAllCarsWithAxios().then(allCarsSuccessHandler).catch(errorHandler);
+    });
+
+    buttonLoadCarAxios.addEventListener('click', (event) => {
+        event.stopPropagation();
+        const carId = retrieveCarId();
+        getCarByIdWithAxios(carId).then(singleCarSuccessHandler).catch(errorHandler);
+    });
+
+    buttonAddCarAxios.addEventListener('click', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        const car = retrieveCarForm();
+        addCarWithAxios(car).then(_ => {
+            buttonLoadCarsAxios.click();
+        });
+    });
+
+    ///////////////////////////
+    // Load data by default  //
+    ///////////////////////////
     const buttonLoadCars = document.getElementById('loadcars');
+    const buttonLoadCar = document.getElementById('loadcar');
+    const buttonAddCar = document.getElementById('add');
+
     buttonLoadCars.addEventListener('click', (event) => {
         event.stopPropagation();
         cleanTable('cars-table');
@@ -21,7 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    const buttonLoadCar = document.getElementById('loadcar');
     buttonLoadCar.addEventListener('click', (event) => {
         event.stopPropagation();
         const carId = retrieveCarId();
@@ -29,7 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
             .then((r) => populateEditCarForm(r));
     });
 
-    const buttonAddCar = document.getElementById('add');
     buttonAddCar.addEventListener('click', (event) => {
         event.stopPropagation();
         event.preventDefault();
@@ -44,3 +78,17 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 });
+
+
+const allCarsSuccessHandler = ({ data }) => {
+    addCarRows(data, 'cars-table');
+}
+
+const singleCarSuccessHandler = ({ data }) => {
+    populateEditCarForm(data);
+}
+
+const errorHandler = (err) => {
+    console.warn("Error -> ", err);
+    return [];
+}
